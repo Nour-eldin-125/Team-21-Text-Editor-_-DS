c80bf66e68d5e5732c4d28f2748c200ed59b62aa
mohamedmakram1 <64763010+mohamedmakram1@users.noreply.github.com>
2022-06-25 21:22:49 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,42 +1,43 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose") 
 const Document = require("./Document")
 
-mongoose.connect("mongodb://localhost/google-docs-clone", {
+//mongoDB connection function  and set our database name to "/google-doc"
+mongoose.connect("mongodb://localhost/google-doc", { 
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
+  useUnifiedTopology: true, //these settings here are just
+  useFindAndModify: false, // for backwards compatibility no matter what thy are
   useCreateIndex: true,
 })
 
-const io = require("socket.io")(3001, {
+const io = require("socket.io")(3001, { //set port number of server to 3001
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
+    origin: "http://localhost:3000", //cleint address
+    methods: ["GET", "POST"], //allow send and receive req
   },
 })
 
-const defaultValue = ""
-
+const defaultValue = "" // set default value to new created doc to empty
+//set socket connection 
 io.on("connection", socket => {
-  socket.on("get-document", async documentId => {
+  //get data from doc with id
+  socket.on("get-document", async documentId => { 
     const document = await findOrCreateDocument(documentId)
-    socket.join(documentId)
-    socket.emit("load-document", document.data)
-
+    socket.join(documentId) //put server in this doc
+    socket.emit("load-document", document.data) //load data
+    //save document 
     socket.on("send-changes", delta => {
       socket.broadcast.to(documentId).emit("receive-changes", delta)
     })
-
+    //update doc data
     socket.on("save-document", async data => {
       await Document.findByIdAndUpdate(documentId, { data })
     })
   })
 })
-
+//find doc by its id
 async function findOrCreateDocument(id) {
   if (id == null) return
-
   const document = await Document.findById(id)
-  if (document) return document
-  return await Document.create({ _id: id, data: defaultValue })
+  if (document) return document //if you find doc with specific id return it to user
+  return await Document.create({ _id: id, data: defaultValue }) //if there is now doc create an empty one
 }
